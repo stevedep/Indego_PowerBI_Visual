5a740646ce0d2fc47615214f6be904be1592e14f
310267217 <310267217@YY191224>
2022-06-25 21:52:41 +0200

    Solution found for timing issue, length needs to be from getTotalLength()

--- src/visual.ts
+++ src/visual.ts
@@ -48,11 +48,7 @@ import VisualObjectInstanceEnumeration = powerbi.VisualObjectInstanceEnumeration
 
 export class Visual implements IVisual {
     private svg: Selection<SVGElement>; //added
-    private div: Selection<SVGElement>; //added
-    private target: HTMLElement;
-    private updateCount: number;
-    private settings: VisualSettings;
-    private textNode: Text;
+
     private visualSettings: VisualSettings;
 
     private container: Selection<SVGElement>;
@@ -71,10 +67,10 @@ export class Visual implements IVisual {
         this.container = this.svg.append("g")
             .classed('container', true);
         this.textValue = this.container.append("text")
-            .classed("textValue", true)            
-        this.xval= this.container.append("text")
             .classed("textValue", true)
-        this.yval= this.container.append("text")
+        this.xval = this.container.append("text")
+            .classed("textValue", true)
+        this.yval = this.container.append("text")
             .classed("textValue", true)
 
 
@@ -104,109 +100,30 @@ export class Visual implements IVisual {
     }
     */
     public update(options: VisualUpdateOptions) {
-
-
-        let width: number = options.viewport.width;
-        let height: number = options.viewport.height;
-        let width_adjusted: number;
-        let height_adjusted: number;
-
-        this.svg.attr("width", width);
-        this.svg.attr("height", height);
-        //let radius: number = Math.min(width, height) / 2.2;
         let dataView: DataView = options.dataViews[0];
-        let DTS = dataView.categorical.values[0].values;      
-        const DTS2 = DTS.filter(n => n);
-        
-      
-
-        let X = dataView.categorical.values[1].values;
-        
-        let Y = dataView.categorical.values[2].values;
-        let X2 = X.filter(n => n);
-        let X3 = X2.slice(2, X2.length - 1);
-        
-        let Y2 = Y.filter(n => n);
-        let Y3 = Y2.slice(2, Y2.length - 1);
-        //debugger;
-        const I = d3.range(X.length);
-        console.log(dataView);
-
         this.visualSettings = VisualSettings.parse<VisualSettings>(dataView);
-        //debugger;
-        let speedsetting: number = Number(this.visualSettings.dataPoint.speed);
+        let width: number = options.viewport.width; let height: number = options.viewport.height;
+        this.svg.attr("width", width); this.svg.attr("height", height);
         let SVG_width2: number = Number(this.visualSettings.dataPoint.SVG_width);
         let SVG_height2: number = Number(this.visualSettings.dataPoint.SVG_height);
 
-        this.textValue
-            .text("Value")
-            .attr("x", "10%")
-            .attr("y", "10%")
-            .attr("dy", "0.35em")
-            .attr("text-anchor", "middle")
-            .style("font-size", 30 + "px");
-        let waitingduration: number = 10000 / DTS2.length; 
-        this.xval
-            .text("Value")
-            .attr("x", "10%")
-            .attr("y", "20%")
-            .attr("dy", "0.35em")
-            .attr("text-anchor", "middle")
-            .style("font-size", 30 + "px");
-        this.yval
-            .text("Value")
-            .attr("x", "20%")
-            .attr("y", "20%")
-            .attr("dy", "0.35em")
-            .attr("text-anchor", "middle")
-            .style("font-size", 30 + "px");
-
-
-
-
-        const sleep = ms => new Promise(r => setTimeout(r, ms));
-      
-        async function loopnumbers() {
-            for (let i = 0; i < DTS.length; i++) {
-            //for (const dt of DTS2) { //use for loop instead of forEach https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
-                await sleep(waitingduration);
-                this.textValue.text(DTS2[i]);
-                this.xval.text(X3[i]);
-                this.yval.text(Y3[i]);
-
-                //this.textValue.text(dt);
-                //console.log(DTS2[i]);
-            }
-        }
-        //debugger;
-        loopnumbers.call(this); //call om the this context mee te geven
-        //this.textValue.text(numbers[0]);
-
+        height = (height > (width * SVG_height2 / SVG_width2)) ? width * SVG_height2 / SVG_width2 : height; 
+        width = (width > (height * SVG_width2 / SVG_height2)) ? width = height * SVG_width2 / SVG_height2 : width;
         
-        //debugger;
-        //this.visualSettings.dataPoint.speed = this.visualSettings.dataPoint.speed * 2;
-       
-        if (height > (width * SVG_height2 / SVG_width2)) {
-            height = width * SVG_height2 / SVG_width2;
-        }
+        let X = dataView.categorical.values[1].values;
+        X.map(x => <number>x);
+        let Y = dataView.categorical.values[2].values;
+        X.map(y => <number>y);
 
-        if (width > (height * SVG_width2 / SVG_height2)) {
-            width = height * SVG_width2 / SVG_height2;
-        }
-
-        let data: [number, number][] = X3.map((e, i) => [<number>e, <number>Y3[i]]);
-        //debugger;
+        let data: [number, number][] = X.map((e, i) => [<number>e, <number>Y[i]]);
 
         this.svg.selectAll("polygon").remove();
         this.svg.selectAll("circle").remove();
         this.svg.selectAll(".circles").remove();
         this.svg.selectAll("path").remove();
 
-
         for (let i = 3; i < dataView.categorical.values.length; i++) {
-
             let Z = dataView.categorical.values[i].values;
-
             let data3 = Z.map(function (d) {
                 if (isNaN(<number>d)) {
                     var f;
@@ -230,82 +147,47 @@ export class Visual implements IVisual {
                 .attr("fill-opacity", 0.3);
         }
 
-        // Construct the line generator.
-        const line = d3.line()
-            .curve(d3.curveCatmullRom)
-            .x(i => <number>(
-                (
-                    (<number>
-                        (i[1]) * (width / SVG_width2))// (8.1 * (width / SVG_width2))) + (20 * (width / SVG_width2))))
-                )
-            ))
-            .y(i => <number>(
-                (
-                    (<number>
-                        (i[0]) * (height / SVG_height2)) //* (8.1 * (height / SVG_height2)) + (30 * (height / SVG_height2)))))
-                )
-            ));
+        const line = d3.line()            
+            .x(i => (i[1]) * (width / SVG_width2))
+            .y(i => (i[0]) * (height / SVG_height2))
 
-        const l = (line(data).length);
-        let test = line(data);
-        //debugger;
+        let L = (line(data));
+        let I = L.length;
 
-
-        async function loopnumbers2() {
-            for (let i = 0; i < 2; i++) {
-                //for (const dt of DTS2) { //use for loop instead of forEach https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
-                
-                console.log(i);
-                let s = function (i) {
-                    switch (i) {
-                        case 0:
-                            return "black";
-                        case 1:
-                            return "red";
-                    }
-                }
-                console.log(s(i));
-                this.svg.append("path")
-                    .datum(data)
-                    .attr("fill", "none")
-                    .attr("stroke", s(i))
-                    .attr("stroke-width", 2.5)
-                    .attr("stroke-linejoin", "round")
-                    .attr("stroke-linecap", "round")
-                    .attr("stroke-dasharray", `0,${l}`)
-                    .attr("d", line)
-                    .transition()
-                    .duration(10000)
-                    .ease(d3.easeLinear)
-                    .attr("stroke-dasharray", `${l},${l}`);
-                await sleep(10000);
-            }
-        }
-
-            loopnumbers2.call(this);
-
-        
-
-
-
-            /*
+        const path = this.svg.append("path")
             .attr("fill", "none")
-            .attr("stroke", "ghostwhite")
+            .attr("stroke", "black")
             .attr("stroke-width", 2)
             .attr("stroke-linejoin", "round")
             .attr("stroke-linecap", "round")
-            .attr("d", line(data))
-            //.interrupt()
+            .attr("d", L);
 
+        // Measure the length of the given SVG path string.
+
+
+        //below was not working, solution found here: 2022-06-25
+        https://stackoverflow.com/questions/21140547/accessing-svg-path-length-in-d3
+
+        /*
+        function length(path) {
+            return (d3.create("svg:path").attr("d", path).node()) .getTotalLength();
+        }
+        */
+        //function animate() {
             
-            .attr("stroke-dasharray", `0,${l}`)
-            .transition()
-            .duration(100000) //X.length * <number>speedsetting)
-            .ease(d3.easeLinear)
-            .attr("stroke-dasharray", `${l},${l}`);
-            */
-      //debugger;  
+               // const l = length(line(I));
+
+                path
+                   // .interrupt()
+                    .attr("stroke-dasharray", function (d) { return "0," + this.getTotalLength(); })
+                    .transition()
+                    .duration(5000)
+                    .ease(d3.easeLinear)
+                    .attr("stroke-dasharray", function (d) { return this.getTotalLength() + "," + this.getTotalLength(); })
+       // }
+
+       // animate();
+
+       // return Object.assign(this.svg.node(), { animate });
     }
-
-
 }
